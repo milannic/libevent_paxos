9512a1155516e8f74923ec2080edf08dbadc009b
Milannic <milannic7@gmail.com>
2014-09-14 23:22:40 -0400

    proxy place holder

---
+++ src/include/proxy/proxy.h
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  proxy.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  09/14/2014 11:20:55 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#ifndef PROXY_H
+#define PROXY_H
+#include "../util/common-header.h"
+
+int place_holder(void);
+
+#endif
---
+++ src/proxy/proxy.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  proxy.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  09/14/2014 11:19:25 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include "../include/proxy/proxy.h"
+
+static int hello(void){
+    return 1;
+};
